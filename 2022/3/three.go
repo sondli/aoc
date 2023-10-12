2b05fc77681e41129ffc70308b4869d926fde742
sondli <sondre.lillelien@gmail.com>
2023-10-12 15:03:16 +0200

    day three done

--- 2022/3/three.go
+++ 2022/3/three.go
@@ -17,38 +17,56 @@ func main() {
 
 	contentRows := strings.Split(string(content), "\n")
 
-    score := 0
+	itemTypes := []rune{}
 
-    for _, contentRow := range contentRows {
-        line := string(contentRow) 
-        splitOn := len(line) / 2 
-        lastIndex := len(line)
-        first := line[0:splitOn]
-        second := line[splitOn:lastIndex]
+	for _, contentRow := range contentRows {
+		line := string(contentRow)
+		splitOn := len(line) / 2
+		lastIndex := len(line)
+		first := line[0:splitOn]
+		second := line[splitOn:lastIndex]
 
-        runes := []rune(first)
-        for i := 0; i < len(first); i++ {
-            rune := runes[i]
-            toAdd := 0
-            if (strings.ContainsRune(second, rune) && toAdd == 0) {
-                fmt.Printf(string(runes) + " " + second + "\n")
-                fmt.Println(string(rune))
-                fmt.Println(getPrio(rune))
-                toAdd = getPrio(rune)
-            }
-            score = score + toAdd
-        }
+		firstRunes := []rune(first)
 
+		for i := 0; i < len(first); i++ {
+			if strings.ContainsRune(second, firstRunes[i]) {
+			    itemTypes = append(itemTypes, firstRunes[i])	
+				break
+			}
+		}
 	}
 
-    fmt.Printf("Answer: %d\n", score) 
+    sum := 0
+    for i := 0; i < len(itemTypes); i++ {
+        sum += getPrio(itemTypes[i])  
+    }
 
+	fmt.Printf("Answer one: %d\n", sum)
+
+    badges := []rune{}
+    
+    for i:= 0; i < len(contentRows) - 2; i += 3 {
+        group := contentRows[i:i + 3]
+        for _, letter := range group[0] {
+           if strings.ContainsRune(group[1], letter) && strings.ContainsRune(group[2], letter) {
+               badges = append(badges, letter) 
+               break
+           }
+        }
+    }
+
+    sumTwo := 0
+    for i := 0; i < len(badges); i++ {
+        sumTwo += getPrio(badges[i])  
+    }
+
+	fmt.Printf("Answer two: %d\n", sumTwo)
 }
 
 func getPrio(char rune) (prio int) {
-    if unicode.IsUpper(char) {
-        return int(char) - 38
-    } else {
-        return int(char) - 96
-    }
+	if unicode.IsUpper(char) {
+		return int(char) - 38
+	} else {
+		return int(char) - 96
+	}
 }
